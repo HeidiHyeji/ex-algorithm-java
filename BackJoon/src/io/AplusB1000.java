b2f89b9b508b75a4741265c1ed7940fac5f8a8ea
KimHyeji <gogo6076@naver.com>
2018-03-07 01:28:17 +0900

    move path

--- BackJoon/src/io/AplusB1000.java
+++
@@ -1,8 +0,0 @@
-package io;
-import java.util.Scanner;
-public class AplusB1000 {
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.println(sc.nextInt()+sc.nextInt());
-	}
-}
---
+++ src/io/AplusB1000.java
@@ -0,0 +1,8 @@
+package io;
+import java.util.Scanner;
+public class AplusB1000 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println(sc.nextInt()+sc.nextInt());
+	}
+}
